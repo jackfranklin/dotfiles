94b834467eddfcfc256be5d0c9491b02c4a34276
Jack Franklin <jack@jackfranklin.net>
2012-10-29 22:36:11 +0000

    updated make script

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 ############################
 # .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
@@ -7,25 +7,16 @@
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim zshrc zshenv zsh"    # list of files/folders to symlink in homedir
-
-##########
-
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
-done
+
+#zsh
+ln -s $dir/.zsh ~/.zsh
+ln -s $dir/.zshenv ~/.zshenv
+ln -s $dir/.zshrc ~/.zshrc
+
+echo "Symlinked Yo"
