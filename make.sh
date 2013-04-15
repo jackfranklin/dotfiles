62a1bd6a2a86d232f6742ed93953abbf8a5c5d17
Jack Franklin <jack@jackfranklin.net>
2013-04-15 10:01:07 +0100

    tidy up make.sh

--- make.sh
+++ make.sh
@@ -12,35 +12,43 @@ dir=$(pwd)                    # dotfiles directory
 echo "Updating bundles"
 $dir/.vim/update_bundles
 
-echo "Installing Command-T"
+echo "Installing Command-T (rbenv presumed)"
 cd $dir/.vim/bundle/command-t/ruby/command-t && rbenv local system && ruby extconf.rb && make
 
 echo "Bundles installed"
+echo "Symlinking"
 
 
 
 #symlinks
 #zsh
+echo "Symlinking ZSH"
 ln -s $dir/.zsh ~/.zsh
 ln -s $dir/.zshenv ~/.zshenv
 ln -s $dir/.zshrc ~/.zshrc
 
 #vim
+echo "Symlinking Vim"
 ln -s $dir/.vim ~/.vim
 ln -s $dir/.vimrc ~/.vimrc
 ln -s $dir/.gvimrc ~/.gvimrc
 
 #tmux
+echo "Symlinking tmux"
 ln -s $dir/.tmux.conf ~/.tmux.conf
 
 #pulldown
+echo "Symlinking pulldown"
 ln -s $dir/.pulldown.json ~/.pulldown.json
 
 #git
+echo "Symlinking git"
 ln -s $dir/.gitconfig ~/.gitconfig
 ln -s $dir/.gitignore_global ~/.gitignore_global
 
 #bin
+echo "Symlinking tmux-vim-select-pane to /usr/bin"
 ln -s $dir/tmux-vim-select-pane /usr/bin/tmux-vim-select-pane
 
-echo "Symlinked Yo"
+echo "All done"
+
