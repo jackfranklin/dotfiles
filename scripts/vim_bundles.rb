36c5a436bd5cf3e09ed5ac689187e5b8178af399
Jack Franklin <jack@jackfranklin.net>
2013-06-03 11:01:03 +0100

    Add nerdtree

--- scripts/vim_bundles.rb
+++ scripts/vim_bundles.rb
@@ -24,6 +24,7 @@ git_bundles = %w{
   git@github.com:tomtom/tlib_vim.git
   git@github.com:honza/vim-snippets.git
   git@github.com:vim-ruby/vim-ruby.git
+  git://github.com/scrooloose/nerdtree.git
 }
 
 bundles_dir = File.join(File.expand_path("~/dotfiles/vim/.vim"), "bundle")
