2e6d0307698fa089ad111e1bb99ffdf35334bde7
Jack Franklin <jack@jackfranklin.net>
2013-06-12 21:44:13 +0100

    add some gems

--- scripts/rbenv_setup.sh
+++ scripts/rbenv_setup.sh
@@ -7,3 +7,8 @@ brew install openssl
 CONFIGURE_OPTS="--with-openssl-dir=`brew --prefix openssl`" rbenv install 2.0.0-p195
 
 rbenv global 1.9.3-p429
+
+# add some gems
+gem install bundler
+gem install rake
+gem install rails
