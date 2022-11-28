d050f08ce2ff55f0ee892dd23907e588ccfc0931
Jack Franklin <jacktfranklin@chromium.org>
2022-11-28 10:47:10 +0000

    winbar tweaks

--- nvim/after/plugin/winbar.lua
+++ nvim/after/plugin/winbar.lua
@@ -5,7 +5,7 @@ require('winbar').setup({
         'startify',
         'terminal',
         'neoterm',
-        'fterm',
+        'FTerm',
         'dashboard',
         'packer',
         'neogitstatus',
