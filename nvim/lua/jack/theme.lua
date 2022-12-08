f0d0859dbb1323fe97d2c83c94bf83755a0d74da
Jack Franklin <jacktfranklin@chromium.org>
2022-12-08 15:28:18 +0000

    change tab style

--- nvim/lua/jack/theme.lua
+++ nvim/lua/jack/theme.lua
@@ -7,9 +7,44 @@ require("catppuccin").setup({
   custom_highlights = function(colors)
     return {
       ["@text.todo"] = { fg = colors.base, bg = colors.yellow, style = { "bold" } },
+      TabLineSel = {
+        bg = colors.green,
+        fg = colors.base,
+      },
     }
   end,
 })
+-- Generate a set of colors to look at:
+-- local latte = require("catppuccin.palettes").get_palette("latte")
+-- local colors = {
+--   base = "#EFF1F5",
+--   blue = "#1e66f5",
+--   crust = "#DCE0E8",
+--   flamingo = "#DD7878",
+--   green = "#40A02B",
+--   lavender = "#7287FD",
+--   mantle = "#E6E9EF",
+--   maroon = "#E64553",
+--   mauve = "#8839EF",
+--   overlay0 = "#9CA0B0",
+--   overlay1 = "#8C8FA1",
+--   overlay2 = "#7C7F93",
+--   peach = "#FE640B",
+--   pink = "#ea76cb",
+--   red = "#D20F39",
+--   rosewater = "#dc8a78",
+--   sapphire = "#209FB5",
+--   sky = "#04A5E5",
+--   subtext0 = "#6C6F85",
+--   subtext1 = "#5C5F77",
+--   surface0 = "#CCD0DA",
+--   surface1 = "#BCC0CC",
+--   surface2 = "#ACB0BE",
+  -- teal = "#179299",
+--   text = "#4C4F69",
+--   yellow = "#df8e1d",
+-- }
+-- print(vim.inspect(latte))
 vim.api.nvim_command("colorscheme catppuccin")
 local theme = vim.api.nvim_cmd({ cmd = "colorscheme" }, { output = true })
 vim.api.nvim_exec(
