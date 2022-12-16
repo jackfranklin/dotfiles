705597f591cd5f8c17e75777509566b0b9bdc87a
Jack Franklin <jack@jackfranklin.net>
2022-12-16 16:20:00 +0000

    Fix not calling FZF setup properly

--- nvim/lua/jack/plugins/fzf-lua.lua
+++ nvim/lua/jack/plugins/fzf-lua.lua
@@ -1,11 +1,11 @@
-vim.api.nvim_set_keymap("n", "<leader>t", ":lua require('fzf-lua').files()<CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>o", ":lua require('fzf-lua').oldfiles()<CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>b", ":lua require('fzf-lua').buffers()<CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>s", ":lua require('fzf-lua').lsp_document_symbols()<CR>", {})
-
 M = {}
 
 M.setup = function(config)
+  vim.api.nvim_set_keymap("n", "<leader>t", ":lua require('fzf-lua').files()<CR>", {})
+  vim.api.nvim_set_keymap("n", "<leader>o", ":lua require('fzf-lua').oldfiles()<CR>", {})
+  vim.api.nvim_set_keymap("n", "<leader>b", ":lua require('fzf-lua').buffers()<CR>", {})
+  vim.api.nvim_set_keymap("n", "<leader>s", ":lua require('fzf-lua').lsp_document_symbols()<CR>", {})
+
   local ignore = { "^node_modules/", "^.git/" }
   local extraIgnores = config.extra_ignore_patterns or {}
   for _, value in ipairs(extraIgnores) do
