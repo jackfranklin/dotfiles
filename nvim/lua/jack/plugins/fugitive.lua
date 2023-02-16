7ac8468af9239071982e16e5a4e0cf8b719d5f7c
Jack Franklin <jack@jackfranklin.net>
2023-02-16 18:47:10 +0000

    fugitive command

--- nvim/lua/jack/plugins/fugitive.lua
+++ nvim/lua/jack/plugins/fugitive.lua
@@ -1,2 +1,3 @@
 vim.api.nvim_set_keymap("n", "<leader>gs", ":tab G<CR>", {})
 vim.api.nvim_set_keymap("n", "<leader>gc", ":Git checkout -b ", {})
+vim.api.nvim_set_keymap("n", "<leader>gp", ":Git push", {})
