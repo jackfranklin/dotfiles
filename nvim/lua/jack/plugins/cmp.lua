58cf93b25f098e0bff4749e3216a10a7c0ef0139
Jack Franklin <jack@jackfranklin.net>
2023-02-18 19:28:48 +0000

    change force pop up mapping

--- nvim/lua/jack/plugins/cmp.lua
+++ nvim/lua/jack/plugins/cmp.lua
@@ -14,7 +14,7 @@ cmp.setup({
   mapping = {
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-l>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
     ["<C-y>"] = cmp.mapping(
       cmp.mapping.confirm({
