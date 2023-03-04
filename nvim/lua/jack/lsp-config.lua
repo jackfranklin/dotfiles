80184b0a5c5da24c9807bba794ff89ae2a9ae5cb
Jack Franklin <jack@jackfranklin.net>
2023-03-04 13:22:19 +0000

    config the new lua ls

--- nvim/lua/jack/lsp-config.lua
+++ nvim/lua/jack/lsp-config.lua
@@ -42,20 +42,14 @@ M.eslint = function(config)
 end
 
 M.lua = function(config)
-  local runtime_path = vim.split(package.path, ";")
-  table.insert(runtime_path, "lua/?.lua")
-  table.insert(runtime_path, "lua/?/init.lua")
-
   nvim_lsp.lua_ls.setup({
-    on_attach = config.on_attach,
     settings = {
       Lua = {
         runtime = {
           -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
           version = "LuaJIT",
-          -- Setup your lua path
-          path = runtime_path,
         },
+
         diagnostics = {
           -- Get the language server to recognize the `vim` global
           globals = { "vim" },
@@ -63,7 +57,6 @@ M.lua = function(config)
         workspace = {
           -- Make the server aware of Neovim runtime files
           library = vim.api.nvim_get_runtime_file("", true),
-          checkThirdParty = false,
         },
         -- Do not send telemetry data containing a randomized but unique identifier
         telemetry = {
