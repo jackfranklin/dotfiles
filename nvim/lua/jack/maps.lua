6162e5350df9ac742bc3e902b011fe5da98fa662
Jack Franklin <jack@jackfranklin.net>
2023-04-16 19:55:03 +0200

    try without this setting for a bit

--- nvim/lua/jack/maps.lua
+++ nvim/lua/jack/maps.lua
@@ -18,10 +18,11 @@ tnoremap <PageDown> <C-\><C-n>:tabnext<CR>
 noremap H ^
 noremap L $
 noremap Y y$
+
 " http://blog.petrzemek.net/2016/04/06/things-about-vim-i-wish-i-knew-earlier/
 " better jk normally but don't remap when it's called with a count
-noremap <silent> <expr> j (v:count == 0 ? 'gj' : 'j')
-noremap <silent> <expr> k (v:count == 0 ? 'gk' : 'k')
+-- noremap <silent> <expr> j (v:count == 0 ? 'gj' : 'j')
+-- noremap <silent> <expr> k (v:count == 0 ? 'gk' : 'k')
 
 " global clipboard
 vnoremap <leader>8 "*y
