56f0e666b4ab1d83c2de2361f783fa9cc6628ebc
Jack Franklin <jack@jackfranklin.net>
2023-06-24 14:13:45 +0100

    fix treesitter text obj

--- nvim/lua/jack/plugins/treesitter.lua
+++ nvim/lua/jack/plugins/treesitter.lua
@@ -6,35 +6,39 @@ require("nvim-treesitter.configs").setup({
   indent = {
     enable = true,
   },
-  select = {
-    enable = true,
-    lookahead = true,
-    keymaps = {
-      -- You can use the capture groups defined in textobjects.scm
-      ["af"] = "@function.outer",
-      ["if"] = "@function.inner",
-      ["ac"] = "@class.outer",
-      ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-      -- You can also use captures from other query groups like `locals.scm`
-      ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-    },
-    -- If you set this to `true` (default is `false`) then any textobject is
-    -- extended to include preceding or succeeding whitespace. Succeeding
-    -- whitespace has priority in order to act similarly to eg the built-in
-    -- `ap`.
-    include_surrounding_whitespace = true,
-  },
-  move = {
-    enable = true,
-    set_jumps = true, -- whether to set jumps in the jumplist
 
-    goto_next_start = {
-      ["]m"] = "@function.outer",
-      ["]]"] = { query = "@class.outer", desc = "Next class start" },
+  textobjects = {
+
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+        -- You can also use captures from other query groups like `locals.scm`
+        ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+      },
+      -- If you set this to `true` (default is `false`) then any textobject is
+      -- extended to include preceding or succeeding whitespace. Succeeding
+      -- whitespace has priority in order to act similarly to eg the built-in
+      -- `ap`.
+      include_surrounding_whitespace = true,
     },
-    goto_previous_start = {
-      ["[m"] = "@function.outer",
-      ["[["] = "@class.outer",
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = { query = "@class.outer", desc = "Next class start" },
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
     },
   },
   incremental_selection = {
