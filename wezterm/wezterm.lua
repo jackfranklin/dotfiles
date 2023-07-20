c58e9e7fc9d717b945e617e02c3195b3d0fba0da
Jack Franklin <jacktfranklin@google.com>
2023-07-20 16:01:04 +0100

    wezterm theme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,18 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 -- This table will hold the configuration.
 local config = {}
 
 -- For example, changing the color scheme:
 config.color_scheme = "Catppuccin Frappe"
+config.color_scheme = "Darkside"
+-- I like the darkside color scheme but the cursor is a bit bright and makes it
+-- hard to see what is under it.
+config.colors = {
+  cursor_bg = "#545252",
+}
 -- config.default_prog = { "C:\\Windows\\System32\\wsl.exe" }
 config.default_domain = "WSL:Ubuntu"
 config.font = wezterm.font("IntelOne Mono")
@@ -18,10 +25,21 @@ config.window_padding = {
 }
 
 config.keys = {
+  -- Shift + Control + R to rename tab
   {
-    key = "r",
-    mods = "CMD|SHIFT",
-    action = wezterm.action.ReloadConfiguration,
+    key = "R",
+    mods = "SHIFT|CTRL",
+    action = act.PromptInputLine({
+      description = "Enter new name for tab",
+      action = wezterm.action_callback(function(window, pane, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    }),
   },
 }
 
