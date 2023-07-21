27104641f7734284c45450671600f03d3e1ba9cd
Jack Franklin <jack@jackfranklin.net>
2023-07-21 08:30:26 +0100

    More subtle matchparen

--- nvim/lua/jack/theme.lua
+++ nvim/lua/jack/theme.lua
@@ -12,6 +12,7 @@ require("dark_flat").setup({
     return {
       ["@include.typescript"] = { fg = colors.white:darken(0.8) },
       ["@punctuation.bracket"] = { fg = colors.pink:darken(0.7) },
+      MatchParen = { fg = colors.white },
     }
   end,
 })
