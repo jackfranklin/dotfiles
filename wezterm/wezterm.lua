313f0ad613988d6666a76ccfe664f9e355f883c8
Jack Franklin <jack@jackfranklin.net>
2023-09-15 15:16:35 +0100

    bump the font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -24,7 +24,7 @@ config.font = wezterm.font("Hack")
 -- Disable ligatures.
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
-config.font_size = 13
+config.font_size = 14
 config.window_padding = {
   left = 0,
   right = 0,
