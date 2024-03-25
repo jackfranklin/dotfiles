66e39d87922d52c20c1cb24a4ffc915dadfc2a04
Jack Franklin <jack@jackfranklin.net>
2024-03-25 22:19:42 +0000

    disable indent in some buftypes

--- nvim/lua/jack/plugins/mini-indent.lua
+++ nvim/lua/jack/plugins/mini-indent.lua
@@ -1,1 +1,14 @@
 require("mini.indentscope").setup({})
+
+-- Disable indents when in terminal, executor windows (nofile) and help
+local augroup = vim.api.nvim_create_augroup("DisableIndent", {})
+vim.api.nvim_clear_autocmds({ group = augroup })
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  group = augroup,
+  callback = function(data)
+    local buftype = vim.api.nvim_get_option_value("buftype", { buf = data.buf })
+    if vim.tbl_contains({ "terminal", "help", "nofile" }, buftype) then
+      vim.b.miniindentscope_disable = true
+    end
+  end,
+})
