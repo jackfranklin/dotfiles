5c69e9679a6d7edb541990d2f8177f807422209e
Jack Franklin <jack@jackfranklin.net>
2024-03-25 23:17:37 +0000

    disable indents in css

--- nvim/lua/jack/plugins/mini-indent.lua
+++ nvim/lua/jack/plugins/mini-indent.lua
@@ -11,7 +11,8 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
   group = augroup,
   callback = function(data)
     local buftype = vim.api.nvim_get_option_value("buftype", { buf = data.buf })
-    if vim.tbl_contains({ "terminal", "help", "nofile" }, buftype) then
+    local filetype = vim.api.nvim_get_option_value("filetype", { buf = data.buf })
+    if vim.tbl_contains({ "terminal", "help", "nofile" }, buftype) or vim.tbl_contains({ "css" }, filetype) then
       vim.b.miniindentscope_disable = true
     end
   end,
