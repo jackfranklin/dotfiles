15cbda140e9f8a98a14b18134d73e138468bd80a
Jack Franklin <jack@jackfranklin.net>
2024-03-29 22:15:33 +0000

    decided I like dimmed inactive windows

--- nvim/lua/jack/theme.lua
+++ nvim/lua/jack/theme.lua
@@ -26,7 +26,7 @@ end
 local function load_kanagawa()
   require("kanagawa").setup({
     undercurl = false, -- TODO: find out how to make this true for Mac only.
-    dimInactive = false,
+    dimInactive = true,
     overrides = function(colors)
       return {
         ["@comment.todo"] = { fg = colors.palette.lotusRed },
