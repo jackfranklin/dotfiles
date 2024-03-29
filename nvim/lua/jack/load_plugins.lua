ccec9ad06fec72c20a122ae619ab4eb36febc586
Jack Franklin <jack@jackfranklin.net>
2024-03-29 22:15:06 +0000

    try cmp signature help for a bit

--- nvim/lua/jack/load_plugins.lua
+++ nvim/lua/jack/load_plugins.lua
@@ -129,6 +129,7 @@ local base_plugins = {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-nvim-lua",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
       "saadparwaiz1/cmp_luasnip",
     },
     config = function()
