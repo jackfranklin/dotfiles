6ec037ca30e41903a853663c3bd4175cb9fffd27
Jack Franklin <jack@jackfranklin.net>
2024-04-13 19:17:00 +0100

    remove cmp signature help

--- nvim/lua/jack/load_plugins.lua
+++ nvim/lua/jack/load_plugins.lua
@@ -150,7 +150,6 @@ local base_plugins = function()
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-nvim-lua",
-        "hrsh7th/cmp-nvim-lsp-signature-help",
         "saadparwaiz1/cmp_luasnip",
       },
       config = function()
