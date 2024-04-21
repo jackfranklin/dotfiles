4a02db91a554b59b4b1d741c6a812260e173f824
Jack Franklin <jack@jackfranklin.net>
2024-04-21 14:12:27 +0100

    create a custom input func

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,12 +6,15 @@ require("jack.statusline")
 require("jack.maps")
 require("jack.folds")
 
+local custom_input = require("jack.custom-input")
+vim.ui.input = custom_input.custom_ui_input
+
 local notes = require("jack.notes")
 
 notes.setup()
 vim.keymap.set("n", "<leader>nd", notes.open_daily_notes, { desc = "[n]ote [d]aily: open note file for current day" })
 vim.keymap.set("n", "<leader>nn", notes.open_note_by_name, { desc = "[n]ote [n]ew: open note file by name" })
 vim.keymap.set("n", "<leader>ng", notes.commit_notes, { desc = "[n]otes [g]it: update and push notes to repo" })
-vim.keymap.set("n", "<leader>nf", notes.list_daily_notes_fzf, { desc = "[n]otes [f]iles: search for notes file" })
+vim.keymap.set("n", "<leader>nl", notes.list_daily_notes_fzf, { desc = "[n]otes [l]ist: search for notes file" })
 vim.keymap.set("n", "<leader>ns", notes.search_daily_notes_fzf, { desc = "[n]otes [s]earch: text search all notes" })
 vim.keymap.set("n", "<leader>nr", notes.open_from_recent_notes, { desc = "[n]otes [r]ecent: list recent note files" })
---
+++ nvim/lua/jack/custom-input.lua
@@ -0,0 +1,48 @@
+local M = {}
+
+M.custom_ui_input = function(opts, callback_func)
+  local width = math.floor(vim.o.columns * 4 / 5)
+
+  local event = require("nui.utils.autocmd").event
+  local Input = require("nui.input")
+  local input_component = Input({
+    relative = "editor",
+    position = "50%",
+    size = {
+      width = width,
+    },
+    border = {
+      style = "rounded",
+      padding = { top = 1, bottom = 1, left = 2, right = 2 },
+      text = {
+        top = opts.prompt or "Input:",
+        top_align = "center",
+      },
+    },
+    win_options = {
+      winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+    },
+  }, {
+    prompt = "> ",
+    default_value = opts.default or "",
+    on_submit = function(value)
+      callback_func(value)
+    end,
+    on_close = function()
+      -- Nothing to do here.
+    end,
+  })
+  -- Make <ESC> close the input
+  input_component:map("n", "<Esc>", function()
+    input_component:unmount()
+  end, { noremap = true })
+  -- Make q close the input.
+  input_component:map("n", "q", function()
+    input_component:unmount()
+  end, { noremap = true })
+  input_component:mount()
+  input_component:on(event.BufLeave, function()
+    input_component:unmount()
+  end)
+end
+return M
