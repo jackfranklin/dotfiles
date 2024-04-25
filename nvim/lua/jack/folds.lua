88bddb101e971cc29ee29913941ba59acb95df61
Jack Franklin <jack@jackfranklin.net>
2024-04-25 23:16:49 +0100

    trying some custom keymaps for folding

--- nvim/lua/jack/folds.lua
+++ nvim/lua/jack/folds.lua
@@ -5,3 +5,14 @@ vim.opt.foldtext = ""
 
 vim.opt.foldnestmax = 3
 vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+
+local function close_all_folds()
+  vim.api.nvim_exec2("%foldc!", { output = false })
+end
+local function open_all_folds()
+  vim.api.nvim_exec2("%foldo!", { output = false })
+end
+
+vim.keymap.set("n", ";C", close_all_folds, { desc = "[C]lose all folds" })
+vim.keymap.set("n", ";O", open_all_folds, { desc = "[O]pen all folds" })
