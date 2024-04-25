c05a35e9e55fdd88f05cdf6bf1de4c18e8e7bf78
Jack Franklin <jack@jackfranklin.net>
2024-04-25 22:11:14 +0100

    experiment with only autofolding test files

--- nvim/lua/jack/folds.lua
+++ nvim/lua/jack/folds.lua
@@ -3,6 +3,19 @@ vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldtext = ""
 
-vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 1
-vim.opt.foldnestmax = 4
+vim.opt.foldnestmax = 3
+
+local augroup = vim.api.nvim_create_augroup("FoldLevel", {})
+vim.api.nvim_clear_autocmds({ group = augroup })
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  group = augroup,
+  callback = function(data)
+    print(vim.inspect(data))
+    local file_name = data.file
+    if string.find(file_name, ".test.", 1, true) then
+      vim.opt.foldlevel = 1
+    else
+      vim.opt.foldlevel = 99
+    end
+  end,
+})
