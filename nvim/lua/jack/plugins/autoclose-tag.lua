214e65d56bc5670e81b455e23f9fb50e60a4cd2a
Jack Franklin <jack@jackfranklin.net>
2024-05-26 22:31:55 +0100

    turn off creation of closing tags automatically

--- nvim/lua/jack/plugins/autoclose-tag.lua
+++ nvim/lua/jack/plugins/autoclose-tag.lua
@@ -1,7 +1,7 @@
+---@diagnostic disable-next-line: missing-fields
 require("nvim-ts-autotag").setup({
   opts = {
-    -- Defaults
-    enable_close = true, -- Auto close tags
+    enable_close = false, -- Auto close tags
     enable_rename = true, -- Auto rename pairs of tags
     enable_close_on_slash = true, -- Auto close on trailing </
   },
