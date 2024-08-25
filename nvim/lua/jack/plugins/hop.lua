0081214395e81c4b0d61c809bfc5f829daf8c7c1
Jack Franklin <jack@jackfranklin.net>
2024-08-25 07:38:51 +0100

    hop keybinds

--- nvim/lua/jack/plugins/hop.lua
+++ nvim/lua/jack/plugins/hop.lua
@@ -1,10 +1,20 @@
 local M = {}
 
 M.setup = function()
-  require("hop").setup({
+  local hop = require("hop")
+  local directions = require("hop.hint").HintDirection
+
+  hop.setup({
     -- This is the default set of keys as suggested in the README
     keys = "etovxqpdygfblzhckisuran",
   })
+
+  vim.keymap.set("n", "<leader>hw", function()
+    hop.hint_words({ direction = directions.AFTER_CURSOR })
+  end, { desc = "[h]op [w]ord forwards" })
+  vim.keymap.set("n", "<leader>hW", function()
+    hop.hint_words({ direction = directions.BEFORE_CURSOR })
+  end, { desc = "[h]op [W]ord backwards" })
 end
 
 return M
