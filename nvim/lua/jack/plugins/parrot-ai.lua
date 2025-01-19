f3788e52bad9775236f3ceb0e16e98854c841f0a
Jack Franklin <jack@jackfranklin.net>
2025-01-19 19:28:37 +0000

    Improved Parrot AI plugin keybindings
    - Renamed `<leader>ln` keybinding to `<leader>lcn` for opening a new chat.
    - Renamed `<leader>lt` keybinding to `<leader>lct` for toggling the chat.
    - Added a new `<leader>lgm` keybinding to generate commit messages using Parrot.

--- nvim/lua/jack/plugins/parrot-ai.lua
+++ nvim/lua/jack/plugins/parrot-ai.lua
@@ -61,11 +61,14 @@ M.setup = function()
       end,
     },
   })
-  vim.keymap.set("n", "<leader>ln", ":PrtChatNew<CR>", {
+  vim.keymap.set("n", "<leader>lcn", ":PrtChatNew<CR>", {
     desc = "Open a new chat with Parrot",
   })
-  vim.keymap.set("n", "<leader>lt<CR>", ":PrtChatToggle<CR>", {
+  vim.keymap.set("n", "<leader>lct<CR>", ":PrtChatToggle<CR>", {
     desc = "Toggle chat with Parrot",
+  })
+  vim.keymap.set("n", "<leader>lgm<CR>", ":PrtCommitMsg<CR>", {
+    desc = "Generate a commit message with Parrot",
   })
 end
 
