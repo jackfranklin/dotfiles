77f7f3f2d6e1d82111d36d736251b85d7d7cd91a
Jack Franklin <jacktfranklin@google.com>
2025-01-20 09:11:30 +0000

    new keybind for executor

--- nvim/lua/jack/plugins/executor.lua
+++ nvim/lua/jack/plugins/executor.lua
@@ -61,6 +61,7 @@ M.setup = function(config)
   normal_key("<leader>er", executor.commands.run, "[r]un")
   normal_key("<leader>ev", executor.commands.toggle_detail, "[v]iew detail")
   normal_key("<leader>es", executor.commands.set_command, "[s]et command")
+  normal_key("<leader>en", executor.commands.run_with_new_command, "[n]ew command and run")
   normal_key("<leader>ep", executor.commands.show_presets, "show [p]resets")
   normal_key("<leader>eh", executor.commands.show_history, "show [h]istory")
 end
