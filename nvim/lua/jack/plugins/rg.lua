498826d486069d7666fff6253e1de668efde7f80
Jack Franklin <jacktfranklin@google.com>
2025-08-27 13:07:16 +0100

    remove filters on rg file

--- nvim/lua/jack/plugins/rg.lua
+++ nvim/lua/jack/plugins/rg.lua
@@ -1,2 +1,2 @@
-vim.g.rg_command = 'rg --vimgrep --smart-case -g "!node_modules/" -g "!locales/" '
+vim.g.rg_command = "rg --vimgrep --smart-case "
 vim.keymap.set("n", "<leader>/", ":Rg<Space>", { desc = "Search with Rg!" })
