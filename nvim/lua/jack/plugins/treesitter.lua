bdbed098152f7a461232a52bdbd7cc0f90542b73
Jack Franklin <jack@jackfranklin.net>
2026-04-07 17:42:34 +0100

    migrating and disabling some plugins I might not use

--- nvim/lua/jack/plugins/treesitter.lua
+++ nvim/lua/jack/plugins/treesitter.lua
@@ -58,110 +58,57 @@ local parsersToInstall = vim
   :totable()
 require("nvim-treesitter").install(parsersToInstall)
 
--- require("nvim-treesitter.configs").setup({
---   ignore_install = {},
---   sync_install = false,
---   auto_install = true,
---   ensure_installed = {
---     "c",
---     "comment",
---     "cpp",
---     "css",
---     "csv",
---     "elm",
---     "fish",
---     "git_config",
---     "git_rebase",
---     "gitattributes",
---     "gitcommit",
---     "gitignore",
---     "html",
---     "javascript",
---     "json",
---     "json5",
---     "jsonc",
---     "lua",
---     "luadoc",
---     "make",
---     "markdown",
---     "markdown_inline",
---     "ninja",
---     "python",
---     "ruby",
---     "rust",
---     "scss",
---     "sql",
---     "ssh_config",
---     "svelte",
---     "tsx",
---     "typescript",
---     "vim",
---     "vimdoc",
---     "yaml",
---   },
---   highlight = {
---     enable = true,
---   },
---   indent = {
---     enable = false,
---   },
+require("nvim-treesitter-textobjects").setup({
+  select = {
+    lookahead = true,
+    include_surrounding_whitespace = true,
+  },
+  move = {
+    set_jumps = true,
+  },
+})
 
---   textobjects = {
---     select = {
---       enable = true,
---       lookahead = true,
---       keymaps = {
---         ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
---         ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
+local select = require("nvim-treesitter-textobjects.select")
+local textobj = function(query)
+  return function()
+    select.select_textobject(query, "textobjects")
+  end
+end
 
---         ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
---         ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
+vim.keymap.set({ "x", "o" }, "aa", textobj("@parameter.outer"), { desc = "Select outer part of a parameter/argument" })
+vim.keymap.set({ "x", "o" }, "ia", textobj("@parameter.inner"), { desc = "Select inner part of a parameter/argument" })
+vim.keymap.set({ "x", "o" }, "ai", textobj("@conditional.outer"), { desc = "Select outer part of a conditional" })
+vim.keymap.set({ "x", "o" }, "ii", textobj("@conditional.inner"), { desc = "Select inner part of a conditional" })
+vim.keymap.set({ "x", "o" }, "al", textobj("@loop.outer"), { desc = "Select outer part of a loop" })
+vim.keymap.set({ "x", "o" }, "il", textobj("@loop.inner"), { desc = "Select inner part of a loop" })
+vim.keymap.set({ "x", "o" }, "af", textobj("@function.outer"), { desc = "Select outer part of a method/function definition" })
+vim.keymap.set({ "x", "o" }, "if", textobj("@function.inner"), { desc = "Select inner part of a method/function definition" })
+vim.keymap.set({ "x", "o" }, "ac", textobj("@class.outer"), { desc = "Select outer part of a class" })
+vim.keymap.set({ "x", "o" }, "ic", textobj("@class.inner"), { desc = "Select inner part of a class" })
 
---         ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
---         ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
+local move = require("nvim-treesitter-textobjects.move")
+local goto_next_start = function(query)
+  return function()
+    move.goto_next_start(query, "textobjects")
+  end
+end
+local goto_prev_start = function(query)
+  return function()
+    move.goto_previous_start(query, "textobjects")
+  end
+end
 
---         ["af"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
---         ["if"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
+vim.keymap.set({ "n", "x", "o" }, "gaf", goto_next_start("@call.outer"), { desc = "Next function call start" })
+vim.keymap.set({ "n", "x", "o" }, "gam", goto_next_start("@function.outer"), { desc = "Next method/function def start" })
+vim.keymap.set({ "n", "x", "o" }, "gac", goto_next_start("@class.outer"), { desc = "Next class start" })
+vim.keymap.set({ "n", "x", "o" }, "gai", goto_next_start("@conditional.outer"), { desc = "Next conditional start" })
+vim.keymap.set({ "n", "x", "o" }, "gal", goto_next_start("@loop.outer"), { desc = "Next loop start" })
 
---         ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
---         ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
---       },
---       -- If you set this to `true` (default is `false`) then any textobject is
---       -- extended to include preceding or succeeding whitespace. Succeeding
---       -- whitespace has priority in order to act similarly to eg the built-in
---       -- `ap`.
---       include_surrounding_whitespace = true,
---     },
---     move = {
---       enable = true,
---       set_jumps = true, -- whether to set jumps in the jumplist
---       goto_next_start = {
---         ["gaf"] = { query = "@call.outer", desc = "Next function call start" },
---         ["gam"] = { query = "@function.outer", desc = "Next method/function def start" },
---         ["gac"] = { query = "@class.outer", desc = "Next class start" },
---         ["gai"] = { query = "@conditional.outer", desc = "Next conditional start" },
---         ["gal"] = { query = "@loop.outer", desc = "Next loop start" },
---       },
---       goto_previous_start = {
---         ["gaF"] = { query = "@call.outer", desc = "Prev function call start" },
---         ["gaM"] = { query = "@function.outer", desc = "Prev method/function def start" },
---         ["gaC"] = { query = "@class.outer", desc = "Prev class start" },
---         ["gaI"] = { query = "@conditional.outer", desc = "Prev conditional start" },
---         ["gaL"] = { query = "@loop.outer", desc = "Prev loop start" },
---       },
---     },
---   },
---   incremental_selection = {
---     enable = true,
---     keymaps = {
---       init_selection = "vv",
---       node_incremental = "+",
---       node_decremental = "=",
---     },
---   },
---   -- silences Lua-LS lint errors.
---   modules = {},
--- })
+vim.keymap.set({ "n", "x", "o" }, "gaF", goto_prev_start("@call.outer"), { desc = "Prev function call start" })
+vim.keymap.set({ "n", "x", "o" }, "gaM", goto_prev_start("@function.outer"), { desc = "Prev method/function def start" })
+vim.keymap.set({ "n", "x", "o" }, "gaC", goto_prev_start("@class.outer"), { desc = "Prev class start" })
+vim.keymap.set({ "n", "x", "o" }, "gaI", goto_prev_start("@conditional.outer"), { desc = "Prev conditional start" })
+vim.keymap.set({ "n", "x", "o" }, "gaL", goto_prev_start("@loop.outer"), { desc = "Prev loop start" })
 
 require("treesitter-context").setup({
   enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
