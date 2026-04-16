d6ed0b9ab03f6b686f8af50f0f7df3365429bfb4
Jack Franklin <jack@jackfranklin.net>
2026-04-16 21:10:01 +0100

    show background tasks in fish

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,5 +2,11 @@ function fish_prompt
     set_color red
     echo -n (basename $PWD)
     set_color normal
+    set -l jobs (suspended-jobs)
+    if test -n "$jobs"
+        set_color yellow
+        echo -n " [$jobs]"
+        set_color normal
+    end
     echo -n ' > '
 end
